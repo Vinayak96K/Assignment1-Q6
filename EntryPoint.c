77a786b6d70fb08193211174b4173373796a0f3e
Vinayak96k <vinayak.patil0304@gmail.com>
2018-08-29 15:29:49 +0530

    first commit

---
+++ EntryPoint.c
@@ -0,0 +1,11 @@
+#include"Myheader.h"
+
+int main(int argc, char const *argv[])
+{
+    int iValue=0;
+    printf("Enter a positive number:");
+    scanf("%d",&iValue);
+    Accept(iValue);
+
+    return 0;
+}
---
+++ Helper.c
@@ -0,0 +1,20 @@
+#include"Myheader.h"
+
+//////////////////////////////////////////////////////////////////////////////////////
+//  Function Name: Accept
+//  Description: Accepts one number from user and print number of * on the screen.
+//  Input: int[IN]
+//  Output: NA
+//  Author: Vinayak Mahendra Patil
+/////////////////////////////////////////////////////////////////////////////////////
+
+void Accept(int iNo)
+{
+    int icnt=0;
+    while(icnt<iNo)
+    {
+        printf("*\t");
+        icnt++;
+    }
+
+}
